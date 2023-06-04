af2c525d4d30108462e3e99150db3c1f96a0232f
IndeokDukeSeo <dukeindream@gmail.com>
2023-06-04 20:07:08 +0900

    #Setting : Corrected build file and yml

---
+++ src/main/java/com/example/moduinterview/interview/ApiInterviewController.java
@@ -0,0 +1,11 @@
+package com.example.moduinterview.interview;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class ApiInterviewController {
+
+}
