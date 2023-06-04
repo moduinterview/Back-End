9468fa7dec273489684c302de0b5ab0a72f4ce59
IndeokDukeSeo <dukeindream@gmail.com>
2023-06-04 19:36:42 +0900

    commit

---
+++ src/test/java/com/example/moduinterview/ModuInterviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.moduinterview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModuInterviewApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
